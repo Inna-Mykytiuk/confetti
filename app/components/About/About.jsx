9da96de3a59de16abd2c09a55d0ed02afeb3b350
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-02-04 12:48:26 +0200

    add animation

--- app/components/About/About.jsx
+++ app/components/About/About.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { motion } from "framer-motion";
 import "./About.css";
 import AboutSlider from "@/app/ui/AboutSlider/AboutSlider";
 const About = () => {
@@ -16,35 +17,71 @@ const About = () => {
           </div>
 
           <ul className='about-list'>
-            <li className='about-item'>
+            <motion.li className='about-item'
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ duration: 0.3 }}
+              variants={{
+                hidden: { opacity: 0, x: -100 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
               <h3>Kreatywne podejście i dbałość o szczegóły</h3>
               <p>
                 Specjaliści zapewniają doradztwo i kreatywność przy wyborze
                 dekoracji balonowych, z dbałością o szczegóły
               </p>
-            </li>
-            <li className='about-item'>
+            </motion.li>
+            <motion.li className='about-item'
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ duration: 0.3 }}
+              variants={{
+                hidden: { opacity: 0, x: 100 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
               <h3>Materiały przyjazne dla środowiska</h3>
               <p>
                 Wykorzystywane są materiały najwyższej jakości, w tym
                 biodegradowalne materiały balonowe, aby pozostawić jak
                 najmniejszy ślad na środowisku.
               </p>
-            </li>
-            <li className='about-item'>
+            </motion.li>
+            <motion.li className='about-item'
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ duration: 0.3 }}
+              variants={{
+                hidden: { opacity: 0, x: -100 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
               <h3>Szeroka gama kolorów do personalizacji</h3>
               <p>
                 Balony i dekoracje są wybierane z szerokiej gamy kolorów, co
                 pozwala spersonalizować wystrój na każdą okazję.
               </p>
-            </li>
-            <li className='about-item'>
+            </motion.li>
+            <motion.li className='about-item'
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ duration: 0.3 }}
+              variants={{
+                hidden: { opacity: 0, x: 100 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
               <h3>Gwarancja dostawy i montażu</h3>
               <p>
                 Firma gwarantuje nie tylko wysokiej jakości dostawę zamówień,
                 ale także ich montaż, zapewniając klientom kompleksową obsługę.
               </p>
-            </li>
+            </motion.li>
           </ul>
           <AboutSlider />
         </div>
--- app/components/Hero/Hero.jsx
+++ app/components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import { motion } from "framer-motion"
 import "./Hero.css";
 import Baloon1 from "../../../public/assets/images/balloon1.png";
 import Baloon2 from "../../../public/assets/images/balloon2.png";
@@ -16,10 +17,19 @@ const Hero = () => {
         className='baloon-img first'
         priority
       />
-      <h1 className='title'>
+      <motion.h1 className='title'
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.3 }}
+        variants={{
+          hidden: { opacity: 0, y: 100 },
+          visible: { opacity: 1, y: 0 },
+        }}
+      >
         Tworzymy unikalne i niepowtarzalne <span>dekoracje balonowe</span> na
         imprezy
-      </h1>
+      </motion.h1>
       <Image
         src={Baloon2}
         alt='air baloon'
