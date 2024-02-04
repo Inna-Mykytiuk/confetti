c2fe536f0c91b184c471006b1f3630aae2a30f27
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-02-04 16:58:55 +0200

    last fixes

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,12 @@
 import "../public/assets/styles/globals.css";
 
 export const metadata = {
+  metadataBase: new URL("https://confetti-tau.vercel.app"),
   title: "Confetti",
   description: "We create unique and unique balloon decorations for events",
   openGraph: {
     images: '../public/assets/images/og-img.png',
   },
-  metadataBase: "https://confetti-tau.vercel.app",
 };
 
 export default function RootLayout({ children }) {
